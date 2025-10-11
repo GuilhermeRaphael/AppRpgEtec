da4c8d274cc4d9893212a2cafb07398efcc9ea94
GuilhermeRaphael <guigaorapha07@gmail.com>
2025-10-10 22:15:41 -0300

    Projeto finalizado e mapa aparecendo com o pin

--- ViewModels/Usuarios/LocalizacaoViewModel.cs
+++ ViewModels/Usuarios/LocalizacaoViewModel.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 using Map = Microsoft.Maui.Controls.Maps.Map;
+
 
 namespace AppRpgEtec.ViewModels.Usuarios
 {
-    public class LocalizacaoViewModel
+    public class LocalizacaoViewModel : BaseViewModel
     {
         private Map meuMapa;
 
@@ -19,8 +22,38 @@ namespace AppRpgEtec.ViewModels.Usuarios
                 if (value != meuMapa)
                 {
                     meuMapa = value;
-                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(MeuMapa));
                 }
+            }
+        }
+
+        public async void InicializarMapa()
+        {
+            try
+            {
+                Location location = new Location(-23.5200241d, -46.596498d);
+
+                Pin pinEtec = new Pin()
+                {
+                    Type = PinType.Place,
+                    Label = "Etec Horacio",
+                    Address = "Rua alcantara, 113, Vila Guilherme",
+                    Location = location,
+                };
+
+                Map map = new Map();
+
+                MapSpan mapSpan = MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(5));
+
+                map.Pins.Add(pinEtec);
+
+                map.MoveToRegion(mapSpan);
+
+                MeuMapa = map;
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Erro", ex.Message, "OK");
             }
         }
     }
